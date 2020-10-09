687e50a088088e570f199255d95dbdba41bbe021
blaszczykkarol <blaszczykkarol70@gmail.com>
2020-10-09 10:11:09 +0200

    Aktualizacja stopki

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -4,17 +4,17 @@ const Footer = () => (
   <footer className="container mx-auto py-16 px-3 mt-48 mb-8 text-gray-800">
     <div className="flex -mx-3">
       <div className="flex-1 px-3">
-        <h2 className="text-lg font-semibold">About Us</h2>
-        <p className="mt-5">Ridiculus mus mauris vitae ultricies leo integer malesuada nunc.</p>
+        <h2 className="text-lg font-semibold">O nas</h2>
+        <p className="mt-5">Zajmujemy się diagnostyką oraz naprawą podespołów do ciągników marki Deutz-Fahr, John Deere itp.</p>
       </div>
       <div className="flex-1 px-3">
-        <h2 className="text-lg font-semibold">Important Links</h2>
+        <h2 className="text-lg font-semibold">Linki</h2>
         <ul className="mt-4 leading-loose">
           <li>
-            <a href="https://codebushi.com">Terms &amp; Conditions</a>
+            <a href="https://www.texapoland.pl/">TexaPoland</a>
           </li>
           <li>
-            <a href="https://codebushi.com">Privacy Policy</a>
+            <a href="https://www.texapoland.pl/">Motor DK</a>
           </li>
         </ul>
       </div>
@@ -22,13 +22,7 @@ const Footer = () => (
         <h2 className="text-lg font-semibold">Social Media</h2>
         <ul className="mt-4 leading-loose">
           <li>
-            <a href="https://dev.to/changoman">Dev.to</a>
-          </li>
-          <li>
-            <a href="https://twitter.com/HuntaroSan">Twitter</a>
-          </li>
-          <li>
-            <a href="https://github.com/codebushi/gatsby-starter-lander">GitHub</a>
+            <a href="https://github.com/blaszczykkarol70">GitHub</a>
           </li>
         </ul>
       </div>
